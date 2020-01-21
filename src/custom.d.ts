dab9aa4a58fefa5dc3ef034fd5adba59c4f070e7
Dong-jin Ahn <adoji92@gmail.com>
2020-01-21 18:39:52 +0900

    Init project

---
+++ src/custom.d.ts
@@ -0,0 +1,10 @@
+// custom.d.ts
+import 'react';
+
+// declaration for styled-jsx tslint
+declare module 'react' {
+  interface StyleHTMLAttributes<T> extends React.HTMLAttributes<T> {
+    jsx?: boolean;
+    global?: boolean;
+  }
+}
