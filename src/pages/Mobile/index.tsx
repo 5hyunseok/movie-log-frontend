12d7ad16821c8608de80da8535f284d49ccb41f7
Dong-jin Ahn <adoji92@gmail.com>
2020-01-22 13:57:07 +0900

    Replace Mobile redirection url

--- src/pages/Mobile/index.tsx
+++ src/pages/Mobile/index.tsx
@@ -2,22 +2,18 @@ import React from 'react';
 export default () => {
   return (
     <div className="wrapper">
-      {' '}
-      <h1 className="title">Coming soon</h1>{' '}
+      <h1 className="title">Coming soon</h1>
       <div className="body">
-        {' '}
         We currently support desktop view only. <br /> Mobile view is coming
-        soon!{' '}
-      </div>{' '}
-      <br />{' '}
-      <a href="https://pixelic.io/">
-        {' '}
+        soon!
+      </div>
+      <br />
+      <a href="https://TODO.YOUR.URL">
         <div className="back-home-btn">
-          {' '}
-          <button>Go to homepage</button>{' '}
-        </div>{' '}
-      </a>{' '}
-      {/* tslint:disable-next-line */}{' '}
+          <button>Go to homepage</button>
+        </div>
+      </a>
+      {/* tslint:disable-next-line */}
       <style jsx>{`
         .wrapper {
           margin-top: 35vh;
@@ -33,7 +29,7 @@ export default () => {
         .back-home-btn {
           margin-top: 2vh;
         }
-      `}</style>{' '}
+      `}</style>
     </div>
   );
 };
